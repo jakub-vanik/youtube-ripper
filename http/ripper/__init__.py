b5ebc0456149d98819b7a735457aecdac78ceab5
Jakub Vanik <jakub.vanik@gmail.com>
2020-04-13 14:40:53 +0200

    Added support for standalone deployment

--- http/ripper/__init__.py
+++ http/ripper/__init__.py
@@ -13,8 +13,11 @@ class ReverseProxied:
     self.script_name = script_name
 
   def __call__(self, environ, start_response):
-    environ["SCRIPT_NAME"] = self.script_name
-    environ["wsgi.url_scheme"] = environ.get("HTTP_X_FORWARDED_PROTO")
+    if self.script_name:
+      environ["SCRIPT_NAME"] = self.script_name
+    scheme = environ.get("HTTP_X_FORWARDED_PROTO")
+    if scheme:
+      environ["wsgi.url_scheme"] = scheme
     return self.app(environ, start_response)
 
 def create_app():
@@ -24,5 +27,5 @@ def create_app():
   app.config["SESSION_COOKIE_HTTPONLY"] = False
   babel.babel.init_app(app)
   app.register_blueprint(main.bp)
-  app.wsgi_app = ReverseProxied(app.wsgi_app, os.environ["SCRIPT_NAME"])
+  app.wsgi_app = ReverseProxied(app.wsgi_app, os.environ.get("SCRIPT_NAME"))
   return app
