5f4cc0ca36d369d04fcde14094a0b7dbb1ee4494
Jakub Vanik <jakub.vanik@gmail.com>
2023-12-15 20:28:13 +0100

    Modified application to work with new Babel version and moved configuration into separate file

--- ripper/__init__.py
+++ ripper/__init__.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
 
+import datetime
 import flask
-import os
+import flask_babel
+import json
+import werkzeug.middleware.proxy_fix
 
-from . import babel
 from . import main
 
-class ReverseProxied:
-
-  def __init__(self, app, script_name):
-    self.app = app
-    self.script_name = script_name
-
-  def __call__(self, environ, start_response):
-    if self.script_name:
-      environ["SCRIPT_NAME"] = self.script_name
-    scheme = environ.get("HTTP_X_FORWARDED_PROTO")
-    if scheme:
-      environ["wsgi.url_scheme"] = scheme
-    return self.app(environ, start_response)
-
-def create_app():
-  app = flask.Flask(__name__)
-  app.config["SECRET_KEY"] = b'\xcbwG\xc8.s\xbe\xf6\xaf`\xc3xL\xa2\x10s'
-  app.config["SESSION_COOKIE_NAME"] = "ripper_session"
-  app.config["SESSION_COOKIE_HTTPONLY"] = False
-  babel.babel.init_app(app)
-  app.register_blueprint(main.bp)
-  app.wsgi_app = ReverseProxied(app.wsgi_app, os.environ.get("SCRIPT_NAME"))
-  return app
+app = flask.Flask(__name__)
+app.config.from_file("config.json", load=json.load)
+app.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(app.wsgi_app, x_proto=1, x_host=1, x_prefix=1)
+flask_babel.Babel(app, locale_selector=lambda: flask.request.accept_languages.best_match(["en", "cs"]))
+app.register_blueprint(main.bp)
